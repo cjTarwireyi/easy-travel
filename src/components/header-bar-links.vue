43a147f3838b312072b8a4d03d70e84ece31ba5a
cornelious <cornelioust@dipar.co.za>
2021-11-28 18:18:53 +0200

    initial commit

---
+++ src/components/header-bar-links.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="navbar-menu">
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a  target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-github fa-2x" aria-hidden="true">{{signIn}}</i>
+          </a>
+          <a  target="_blank" rel="noopener noreferrer">
+            <i class="" aria-hidden="true">{{signOut}}</i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      signIn: 'Log in',
+      signOut: 'Sign Up',
+    };
+  },
+};
+</script>
