4124a900b893f77b7c45670033a496290f240630
cornelious <cornelioust@dipar.co.za>
2021-12-14 20:06:35 +0200

    added passenger component, added 2 way data binding

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,18 @@
   <div id="app">
     <HeaderBar />
     <div class="main-section content-title-group">
-     
+     <Passengers/>
     </div>
   </div>
 </template>
 
 <script>
 import HeaderBar from '@/components/header-bar';
+import Passengers from '@/components/passengers';
 
 export default {
   name: 'App',
-  components: { HeaderBar },
+  components: { HeaderBar,Passengers },
 };
 </script>
 
---
+++ src/components/passengers.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="content-container">
+    <div class="section content-title-group">
+      <h2 class="title">Passengers</h2>
+    </div>
+    <div class="columns">
+      <div class="column is-8">
+        <div class="card edit-detail">
+          <header class="card-header">
+            <p class="card-header-title"></p>
+          </header>
+          <div class="card-content">
+            <div class="content">             
+              <div class="field">
+                <label class="label" for="firstName">first name</label>
+                <input class="input" id="firstName" v-model="passenger.firstName" />
+              </div>
+              <div class="field">
+                <label class="label" for="lastName">last name</label>
+                <input class="input" id="lastName" v-model="passenger.lastName" />
+              </div>
+              <div class="field">
+                <label class="label" for="idNumber">ID No</label>
+                <input class="input" id="idNumber" type="text" v-model="passenger.IdNumber"/>
+              </div>
+             <div class="field">
+                <label class="label" for="dateOfBirth">DOB</label>
+                <input class="input" id="dateOfBirth" type="text" v-model="passenger.dateOfBirth" />
+              </div>
+               <div class="field">
+                <label class="label">Gender</label>
+                <label class="radio" for="female">
+                  <input type="radio" id="female" value="female" />
+                  Female
+                </label>
+                <label class="radio" for="male">
+                  <input type="radio" id="male" value="male" />
+                  Male
+                </label>               
+                <div class="color-line"></div>
+              </div>
+              <div class="field">
+                <label for="power">
+                  Departure
+                  <div class="select is-primary">
+                    <select id="departure" v-model="passenger.departure">
+                      <option disabled value>Please select one</option>
+                      <option>Cape Town CBD</option>
+                      <option>Cape Town Bellville</option>
+                      <option>Joburg</option>
+                      <option>Musina</option>
+                    </select>
+                  </div>
+                </label>
+              </div>
+              <div class="field">
+                <label for="power">
+                  Destination
+                  <div class="select is-primary">
+                    <select id="destination">
+                      <option disabled value>Please select one</option>
+                      <option>Cape Town CBD</option>
+                      <option>Cape Town Bellville</option>
+                      <option>Joburg</option>
+                      <option>Musina</option>
+                    </select>
+                  </div>
+                </label>
+              </div>
+              <div class="field">
+                <label class="checkbox" for="active">
+                  active
+                  <input type="checkbox" class="is-primary" id="active" v-model="passenger.active" />
+                </label>
+              </div>
+            </div>
+          </div>
+          <footer class="card-footer">
+            <button class="link card-footer-item cancel-button" @click="cancel()">
+              <i class="fas fa-undo"></i>
+              <span>Cancel</span>
+            </button>
+            <button class="link card-footer-item" @click="save()">
+              <i class="fas fa-save"></i>
+              <span>Save</span>
+            </button>
+          </footer>
+        </div>
+        <div class="notification is-info">{{message}}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Passengers',
+  data(){
+    return{
+      passenger:{
+        firstName:'John',
+        lastName:'Dor',
+        IdNumber:'12345',
+        dateOfBirth:'',
+        gender:'M',
+        active:true,
+        departure:'Cape Town CBD'
+      },
+        message:'',
+    }
+  },
+
+  methods: {
+    cancel() {
+      this.message='';
+    },
+    save() {
+      this.message = JSON.stringify(this.passenger,null,'\n');
+    }
+
+  },
+};
+</script>
