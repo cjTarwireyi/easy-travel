ffd4a8cd919cd64614e259665fc18fdc76eb3e37
cornelious <cornelioust@dipar.co.za>
2021-12-17 13:10:34 +0200

    nesting passenger detail component, passing values to child component

--- src/components/passengers.vue
+++ src/components/passengers.vue
@@ -18,102 +18,9 @@
         </ul>
         </div>
             <div class="notification is-info" v-show="message">{{ message }}</div>
-      </div>
-    
+      </div>    
     </div>
-    <div class="columns" v-if="selectedPassenger">
-      <div class="column is-3">
-           <div class="card">
-        <div class="card edit-detail">
-          <header class="card-header">
-            <p class="card-header-title">{{fullName}} </p>
-          </header>
-          <div class="card-content">
-            <div class="content">             
-              <div class="field">
-                <label class="label" for="firstName">first name</label>
-                <input class="input" id="firstName" v-model="selectedPassenger.firstName" />
-              </div>
-              <div class="field">
-                <label class="label" for="lastName">last name</label>
-                <input class="input" id="lastName" v-model="selectedPassenger.lastName" />
-              </div>
-              <div class="field">
-                <label class="label" for="idNumber">ID No</label>
-                <input class="input" id="idNumber" type="text" v-model="selectedPassenger.IdNumber"/>
-              </div>
-             <div class="field">
-                <label class="label" for="dateOfBirth">DOB</label>
-                <input class="input" id="dateOfBirth" type="text" v-model="selectedPassenger.dateOfBirth" />
-              </div>
-               <div class="field">
-                <label class="label">Gender</label>
-                <label class="radio" for="female">
-                  <input type="radio" id="female" value="female" v-model="selectedPassenger.gender"/>
-                  Female
-                </label>
-                <label class="radio" for="male">
-                  <input type="radio" id="male" value="male" v-model="selectedPassenger.gender" />
-                  Male
-                </label>               
-                <div class="color-line"></div>
-              </div>
-              <div class="field">
-              <div class="columns">
-                <label class="column">
-                  Departure
-                 
-                </label>
-                 <div class="select is-primary column">
-                    <select id="departure" v-model="selectedPassenger.departure" :class="{invalid: !selectedPassenger.departure}" @keyup.esc="clearDeparture()">
-                      <option disabled value>Please select one</option>
-                      <option>Cape Town CBD</option>
-                      <option>Cape Town Bellville</option>
-                      <option>Joburg</option>
-                      <option>Musina</option>
-                    </select>
-                  </div>
-                  </div>
-              </div>
-              <div class="field">
-                 <div class="columns">
-                <label class="column">
-                  Destination                 
-                </label>
-                 <div class="select is-primary column">
-                    <select id="destination" v-model="selectedPassenger.destination" :class="{invalid: !selectedPassenger.destination}" @keyup.esc="clearDestination()">
-                      <option disabled value>Please select one</option>
-                      <option>Cape Town CBD</option>
-                      <option>Cape Town Bellville</option>
-                      <option>Joburg</option>
-                      <option>Musina</option>
-                    </select>
-                  </div>
-                  </div>
-              </div>
-              <div class="field">
-                <label class="checkbox" for="active">
-                  active
-                  <input type="checkbox" class="is-primary" id="active" v-model="selectedPassenger.active" />
-                </label>
-              </div>
-            </div>
-          </div>
-          <footer class="card-footer">
-            <button class="link card-footer-item cancel-button" @click="cancel()">
-              <i class="fas fa-undo"></i>
-              <span>Cancel</span>
-            </button>
-            <button class="link card-footer-item" @click="save()">
-              <i class="fas fa-save"></i>
-              <span>Save</span>
-            </button>
-          </footer>
-        </div>
-        <div class="notification is-info"></div>
-      </div>
-      </div>
-    </div>
+    <PassengerDetail v-if="selectedPassenger" :passenger = "selectedPassenger"/>  
   </div>
 </template>
 
@@ -170,7 +77,10 @@ const mockApiPassengers = [
         },
       ];
 
+import PassengerDetail from '@/components/passenger-detail';
 export default {
+  name: 'Passengers',
+  components:{PassengerDetail},
   name: 'Passengers',
   data(){
     return{
@@ -178,11 +88,7 @@ export default {
       passengers:[]
     }
   },
-    computed: {
-      fullName() {
-        return `${this.selectedPassenger.firstName} ${this.selectedPassenger.lastName}`; 
-      }
-    },
+  
     created () {
       this.loadPassengers();
     },
@@ -198,12 +104,7 @@ export default {
       this.passengers = await this.getPassengers();
       this.message = '';
     },
-    cancel() {
-      this.message='';
-    },
-    save() {
-      this.message = JSON.stringify(this.selectedPassenger,null,'\n');
-    },
+   
     clearDeparture(){
       this.selectedPassenger.departure='';
     },
